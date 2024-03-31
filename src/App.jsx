ea6d77d7dda2bb1228018e6f254dc461271f271f
yashvichauhan <31180761+yashvichauhan@users.noreply.github.com>
2024-03-30 23:17:34 -0400

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import StudentForm from "./StudentForm";
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <h3>The student1 application</h3>
+    <h3>The student1 application test</h3>
+    <StudentForm/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
+import 'semantic-ui-css/semantic.min.css';
+import {  FormField, Button, Checkbox, Form, Input, Select } from 'semantic-ui-react'
+
+const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+    { key: 'o', text: 'Other', value: 'other' },
+]
 
 const StudentForm = () => {
+
+    const [firstName,setFirstName] = useState('');
+    const [email,setEmail] = useState('');
+    const [gender, setGender] = useState('male');
+    const [phoneNumber, setPhoneNumber] = useState(0);
+
+    const handleSubmit =() =>{
+        console.log(firstName, email, gender, phoneNumber);
+    }
+
     return(
-        <h6>This is a student form</h6>
+        <Form onSubmit={handleSubmit} > 
+                <FormField
+                    control={Input}
+                    label='Full name'
+                    value={firstName}
+                    onChange= {(e) => setFirstName(e.target.value)}
+                    placeholder='Full name'
+                />
+                <FormField
+                    control={Input}
+                    label='Email'
+                    value={email}
+                    onChange= {(e) => setEmail(e.target.value)}
+                    options={options}
+                    placeholder='email'
+                />
+                <FormField
+                    control={Select}
+                    label='Gender'
+                    value={gender}
+                    options={options}
+                    onChange= {(e,data) => setGender(data.value)}
+                    placeholder='Gender'
+                />
+                <FormField
+                    control={Input}
+                    label='Phone number'
+                    value={phoneNumber}
+                    onChange= {(e) => setPhoneNumber(e.target.value)}
+                    placeholder='+1123456789'
+                />
+            <FormField>
+                <Checkbox label='I agree to the Terms and Conditions' />
+            </FormField>
+            <Button type='submit'>Submit</Button>
+        </Form>
     )
 }
 
